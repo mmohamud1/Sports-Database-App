4eba8e5263421513fa21d4178cce2475c2b5991e
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-08-28 18:52:46 +0200

    Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Sports from './pages/Sports';
 
 const App = () => {
+
+  // Get list of all sports
+  const listSports = async () => {
+    try {
+      const res = await axios.get(`https://www.thesportsdb.com/api/v1/json/1/all_sports.php`)
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <Router>
       <div>
@@ -13,7 +26,8 @@ const App = () => {
         <Header />
         <Switch>
           <Route exact path='/' component={Home} />
-        </Switch>
+          <Route exact path='/sports' component={Sports} />
+        </Switch>  
       </div>
     </Router>  
   );
