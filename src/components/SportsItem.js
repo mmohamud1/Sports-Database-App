d2c5439e196df52f9b4c43f91d12a182dc6bbbe2
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-08-30 16:32:31 +0200

    Commit

--- src/components/SportsItem.js
+++ src/components/SportsItem.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import Logo from '../img/sportsicon.png'
 
 const SportsItem = ({ sport }) => {
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+
     return (
         <div>
             <div className="card-1">
@@ -9,11 +13,20 @@ const SportsItem = ({ sport }) => {
                 </div>
                 <div className="card-1-bottom">
                     <h1 className="card-1-title">{sport.strSport}</h1>
-                    <button className="btn card-1-btn">
+                    <button className="btn card-1-btn" onClick={() => setModalIsOpen(true)}>
                         More Info
                     </button>
                 </div>
             </div>
+            <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} className="modal-2">
+                <button className="close-btn" onClick={() => setModalIsOpen(false)} >
+                    <i className="fa fa-times"></i>
+                </button>
+                <div className="modal-2-header">
+                    <img src={Logo} alt="logo" style={{ width: '55px', height: '50px'}}/>
+                    <h3>{sport.strSport}</h3>
+                </div>
+            </Modal>
         </div>
     )
 }
