6a644eb2cc4a2c69fe160d3b968256392e6a6a0d
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-08-31 00:06:02 +0200

    Commit

--- src/pages/Teams.js
+++ src/pages/Teams.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import TeamSearch from '../components/TeamSearch'
-import Spinner from '../components/Spinner'
 
-const Teams = ({ getTeams, teams, loading }) => {
+
+const Teams = ({ getTeams, teams }) => {
 
     return (
-        <div>
+        <div className="team-bg">
             <TeamSearch getTeams={getTeams}/>
-            {loading ? (
-                <Spinner />
-            ) : (
-                <div className="team-card">
-                    <div className="team-card-top">
-                        <img src={teams} alt=""/>
-                    </div>
-                </div>
-            )}
+            <div className="grid-3 teams-container">
+                {teams.map(team => (
+                    <img key={team.idTeam} src={team.strTeamBadge} alt="Team Badge" style={{ width: '100px', height: '100px'}}/>
+                ))}
+            </div>
         </div>
     )
 }
