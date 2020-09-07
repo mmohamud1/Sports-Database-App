d87824ab4e85cd455cccb1483c1e993f4f59095f
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-09-07 19:39:32 +0200

    Commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,15 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="footer">
-            
+            <ul className="footer-nav">
+                <li className="footer-nav-item"><p className="footer-nav-link">Request Sport</p></li>
+                <li className="footer-nav-item"><p className="footer-nav-link">Request Team</p></li>
+                <li className="footer-nav-item"><p className="footer-nav-link">Request Player</p></li>
+                <li className="footer-nav-item"><p className="footer-nav-link">Contact Us</p></li>
+            </ul>
+            <p class="footer-copyright">
+                &copy; Copyright 2020 by Mohamed Mohamud. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero officiis saepe doloribus fuga placeat blanditiis qui at quod, possimus delectus incidunt! Veniam totam saepe, officia ex debitis quisquam quam!
+            </p>
         </footer>
     )
 }
