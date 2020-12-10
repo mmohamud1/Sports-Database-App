13c8043d0ab8bdfd813cc44d4c9b130ddc57ded5
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-10 12:12:40 +0000

    Commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 const Header = () => {
-    // toggle nav button 
-    const toggle = () => {
-        document.body.classList.toggle('show-nav')
-    }
+  // toggle nav button
+  const toggle = () => {
+    document.body.classList.toggle('show-nav');
+  };
 
-    return (
-        <header className="header">
-            <button className="toggle" onClick={toggle}>
-                <i className="fa fa-bars fa-2x"></i>
-            </button>
-            <h1>Sports Database</h1>
-        </header>
-    )
+  return (
+    <header className='header'>
+      <button className='toggle' onClick={toggle}>
+        <i className='fa fa-bars fa-2x'></i>
+      </button>
+      <h1>Sports Database</h1>
+    </header>
+  );
 };
 
 export default Header;
