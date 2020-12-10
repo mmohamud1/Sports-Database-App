b1566f87fd374a7c68e6371f9fe57c31bc06db59
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-10 12:11:33 +0000

    Commit

--- src/pages/Players.js
+++ src/pages/Players.js
@@ -4,43 +4,43 @@ import Spinner from '../components/Spinner';
 import PlayerItem from '../components/PlayerItem';
 
 const Players = ({ getPlayers, players, loading, clearSearch }) => {
-    // Show spinner while fetching Api info
-    if (loading) {
-        return (
-            <div className="team-bg">
-                <PlayerSearch getPlayers={getPlayers} />
-                <Spinner />        
-            </div>
-        )
+  // Show spinner while fetching Api info
+  if (loading) {
+    return (
+      <div className='team-bg'>
+        <PlayerSearch getPlayers={getPlayers} />
+        <Spinner />
+      </div>
+    );
     // Show error message if there is no result for user input and clear btn
-    } else if (players === null) {
-        return (
-            <div className="team-bg">
-                <PlayerSearch getPlayers={getPlayers} />
-                <button className="clear-btn"  onClick={clearSearch}>
-                    <i className="fa fa-times"></i> Clear Search
-                </button>
-                <h1 className="error-msg">no results</h1>      
-            </div> 
-        )
+  } else if (players === null) {
+    return (
+      <div className='team-bg'>
+        <PlayerSearch getPlayers={getPlayers} />
+        <button className='clear-btn' onClick={clearSearch}>
+          <i className='fa fa-times'></i> Clear Search
+        </button>
+        <h1 className='error-msg'>no results</h1>
+      </div>
+    );
     // Show cards for players searched and clear btn
-    } else {
-        return (
-            <div className="team-bg">
-               <PlayerSearch getPlayers={getPlayers} />
-               {players.length > 0 && (
-                    <button className="clear-btn"  onClick={clearSearch}>
-                        <i className="fa fa-times"></i> Clear Search
-                    </button>
-                )}
-               <div className="grid-3 teams-container">
-                   {players.map(player => (
-                       <PlayerItem key={player.idPlayer} player={player}/>
-                   ))}
-               </div>
-            </div>
-        );
-    };
+  } else {
+    return (
+      <div className='team-bg'>
+        <PlayerSearch getPlayers={getPlayers} />
+        {players.length > 0 && (
+          <button className='clear-btn' onClick={clearSearch}>
+            <i className='fa fa-times'></i> Clear Search
+          </button>
+        )}
+        <div className='grid-3 teams-container'>
+          {players.map((player) => (
+            <PlayerItem key={player.idPlayer} player={player} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 };
 
 export default Players;
