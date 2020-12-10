9499ce39536f4355942b625b9d1852693a7c7c33
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-10 12:13:05 +0000

    Commit

--- src/components/PlayerSearch.js
+++ src/components/PlayerSearch.js
@@ -1,32 +1,38 @@
 import React, { useState } from 'react';
 
 const PlayerSearch = ({ getPlayers }) => {
-    const [text, setText] = useState('');
+  const [text, setText] = useState('');
 
-    const onSubmit = e => {
-        e.preventDefault();
-        if (text === '') {
-            alert('Please enter something');
-        } else {
-            getPlayers(text);
-            setText('');
-        }
-    };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (text === '') {
+      alert('Please enter something');
+    } else {
+      getPlayers(text);
+      setText('');
+    }
+  };
 
-    const onChange = e => setText(e.target.value);
+  const onChange = (e) => setText(e.target.value);
 
-    return (
-        <div>
-            <form onSubmit={onSubmit} className="search-box">
-                <div className="search-bar">
-                    <input type="text" className="search-input" placeholder="Search Players" value={text} onChange={onChange}/>
-                    <button className="search-button">
-                        <i className="fas fa-search search-icon"></i>
-                    </button>
-                </div>
-            </form>
+  return (
+    <div>
+      <form onSubmit={onSubmit} className='search-box'>
+        <div className='search-bar'>
+          <input
+            type='text'
+            className='search-input'
+            placeholder='Search Players'
+            value={text}
+            onChange={onChange}
+          />
+          <button className='search-button'>
+            <i className='fas fa-search search-icon'></i>
+          </button>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default PlayerSearch;
