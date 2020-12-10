d8fb9f6655ca23997fe5cc36056d65a7e7cadd26
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-10 12:12:47 +0000

    Commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,27 @@ import Logo from '../img/sportsicon.png';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    return (
-        <nav>
-            <div className="logo">
-                <img src={Logo} alt="Logo"/>
-            </div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/sports">Sports</Link></li>
-                <li><Link to="/teams">Teams</Link></li>
-                <li><Link to="/players">Players</Link></li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav>
+      <div className='logo'>
+        <img src={Logo} alt='Logo' />
+      </div>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/sports'>Sports</Link>
+        </li>
+        <li>
+          <Link to='/teams'>Teams</Link>
+        </li>
+        <li>
+          <Link to='/players'>Players</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
