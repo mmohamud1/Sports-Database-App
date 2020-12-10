65a0bf48429fde4de17a92bd1b6894f8170666a0
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-10 12:11:43 +0000

    Commit

--- src/pages/Sports.js
+++ src/pages/Sports.js
@@ -3,23 +3,23 @@ import SportsItem from '../components/SportsItem';
 import Spinner from '../components/Spinner';
 
 const Sports = ({ listSports, sports, loading }) => {
-    // Get sports data as component mounts
-    useEffect(() => {
-        listSports();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, []);
+  // Get sports data as component mounts
+  useEffect(() => {
+    listSports();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    if (loading) {
-        return <Spinner />
-    } else {
-        return (
-            <div className="grid-4 my-3 px-2">
-                {sports.map(sport => (
-                    <SportsItem key={sport.idSport} sport={sport}/>
-                ))}
-            </div>
-        );
-    };  
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div className='grid-4 my-3 px-2'>
+        {sports.map((sport) => (
+          <SportsItem key={sport.idSport} sport={sport} />
+        ))}
+      </div>
+    );
+  }
 };
 
 export default Sports;
