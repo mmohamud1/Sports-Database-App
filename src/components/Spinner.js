55b0eb749c3565afe4d7a3d1baa6abc3bd8e35a4
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-12-10 12:13:33 +0000

    Commit

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,11 +2,15 @@ import React from 'react';
 import spinner from '../img/spinner 2.gif';
 
 const Spinner = () => {
-    return (
-        <div>
-            <img src={spinner} style={{ width: '100px', margin: '100px auto', display: 'block'}} alt='loading'/>
-        </div>
-    );
+  return (
+    <div>
+      <img
+        src={spinner}
+        style={{ width: '100px', margin: '100px auto', display: 'block' }}
+        alt='loading'
+      />
+    </div>
+  );
 };
 
 export default Spinner;
